a729ba3f2ec8fc743940efec65821f9d44b53159
kushal goel <kgoel8451@gmail.com>
2025-02-27 14:42:42 +0530

    Fixing deployment issues

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,18 +1,17 @@
-import jwt from "jsonwebtoken";
-
-export const genrateToken = (userId, res) => {
+export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
     expiresIn: "7d",
   });
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    // cross site scrpting attack
+    httpOnly: true, 
     sameSite: "strict", 
-    // Cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    secure: true, 
   });
+
+  console.log("Token sent:", token);
+  console.log("Set-Cookie Header:", res.getHeaders()["set-cookie"]);
 
   return token;
 };
